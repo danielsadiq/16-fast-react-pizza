d8b8946a4d77bc6ae6661df5c5ee3f2bf1973f2a
Daniel Sadiq <danielsadiq93@gmail.com>
2024-10-11 14:21:56 +0100

    cart done

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,104 +3,96 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import { useSelector } from "react-redux";
+import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
+import store from "../../store";
+import EmptyCart from "../cart/EmptyCart";
+import {formatCurrency} from "../../utilities/helpers";
+import { useState } from "react";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function CreateOrder() {
-  // const [withPriority, setWithPriority] = useState(false);
+  const [withPriority, setWithPriority] = useState(false);
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
-  const cart = fakeCart;
-  const username = useSelector(state => state.user.username);
+  const cart = useSelector(getCart);
+  const username = useSelector((state) => state.user.username);
+  const totalCartPrice = useSelector(getTotalCartPrice());
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return < EmptyCart/>
 
   return (
     <div className="px-4 py-6">
-      <h2 className="text-xl font-semibold mb-8">Ready to order? Let{`'`}s go!</h2>
+      <h2 className="mb-8 text-xl font-semibold">
+        Ready to order? Let{`'`}s go!
+      </h2>
 
       {/* <Form method="POST" action="/order/new"> */}
-      <Form method="POST" >
-        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center justify-between">
+      <Form method="POST">
+        <div className="mb-5 flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input 
-            className="input grow" 
+          <input
+            className="input grow"
             type="text"
             defaultValue={username}
-            name="customer" 
-            required />
+            name="customer"
+            required
+          />
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center justify-between">
+        <div className="mb-5 flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
           <div className="grow">
-            <input 
-              className="input w-full"
-              type="tel" 
-              name="phone" 
-              required />
-          
-          {formErrors?.phone && <p className="text-xs mt-2 text-red-700 bg-red-100 p-2 rounded">{formErrors.phone}</p>}
+            <input className="input w-full" type="tel" name="phone" required />
+
+            {formErrors?.phone && (
+              <p className="mt-2 rounded bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center justify-between"> 
+        <div className="mb-5 flex flex-col justify-between gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
-              type="text" 
-              name="address" 
-
-              required />
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div className="flex gap-2 mb-12">
+        <div className="mb-12 flex gap-2">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority" className="font-meium">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-meium">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           {/* This is to pass data to the form in a dynamic way */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? "Placing order" : "Order now"}
+            {isSubmitting ? "Placing order" : `Order now for ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -109,23 +101,27 @@ function CreateOrder() {
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
-export async function action({request}){
+export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  
-  const order = {...data,
+
+  const order = {
+    ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on"
-  }
+    priority: data.priority === "true",
+  };
   const errors = {};
-  if (!isValidPhone(order.phone)) errors.phone = "Please give us a valid phone number. We might need it to contact you"
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "Please give us a valid phone number. We might need it to contact you";
   // To check if the errors object is empty
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect.
   const newOrder = await createOrder(order);
-
-  return redirect(`/order/${newOrder.id}`)
+  // DO NOT overuse
+  store.dispatch(clearCart());
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -6,6 +6,7 @@ function Button({children, disabled, to, type, onClick}) {
     const styles = {
         primary: base + " py-3 px-4 md:px-6 md:py-4",
         small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xsm",
+        round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
         secondary: "text-sm border-2 border-stone-300 font-semibold text-stone-400  inline-block tracking-wide rounded-full uppercase hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:ring focus:bg-stone-300 focus:ring-offset-2 focus:bg-yellow-300 focus:text-stone-800 focus:ring-stone-200 focus:outline-none disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5",
     }
 
